7e58a0856e1fa3a54c9e89ab097b429a8dcec7f9
GaoJ99 <126736291+GaoJ99@users.noreply.github.com>
2023-03-02 11:50:24 +0800

    Update rl_Qtable.py

--- DTI_QL/rl_Qtable.py
+++ DTI_QL/rl_Qtable.py
@@ -9,35 +9,27 @@ class QLearningTable:
         self.gamma = reward_decay
         self.epsilon = e_greedy
         self.action_spaces = action_spaces
-        self.q_table = pd.DataFrame(columns=self.actions, dtype=np.float64)  # 生成q_table
+        self.q_table = pd.DataFrame(columns=self.actions, dtype=np.float64) 
 
     def choose_action(self, observation, count):
-        self.check_state_exist(str(observation))  # 检测当前状态是否存在于Q表中，没有的话就在Q表中增加这个节点
-        # 动作选择
-        # while True:
-        if np.random.uniform() > self.epsilon**count:  # epsilon等于0.9, 90%概率Q函数决定行动
-            # 选择最佳的动作
-            # print("self.epsilon**count:", self.epsilon**count)
-            state_action = self.q_table.loc[str(observation), :]  # 取出当前observation所在的所有动作
+        self.check_state_exist(str(observation))  
 
-            # 当最大Q值对应多个动作时，随机选择动作
-            action = np.random.choice(state_action[state_action == np.max(state_action)].index)  # 返回的是一个数字
+        if np.random.uniform() > self.epsilon**count: 
+            state_action = self.q_table.loc[str(observation), :]  
+            action = np.random.choice(state_action[state_action == np.max(state_action)].index) 
         else:
-            # 10%概率随机选择一个动作，探索其他可能
-            # while True:
             action = np.random.choice(self.actions)
 
         return action
 
     def learn(self, s, a, r, s_, done):
-        # self.check_state_exist(s_)  # 检查是否存在下一个状态
-        q_predict = self.q_table.loc[s, a]  # 获得当前状态，当前动作对应的预测分数
-        if not done:  # done为True表示下个状态不是终点
-            self.check_state_exist(s_)  # 检查是否存在下一个状态
+        q_predict = self.q_table.loc[s, a] 
+        if not done: 
+            self.check_state_exist(s_) 
             q_target = r + self.gamma * self.q_table.loc[s_, :].max()
         else:
             q_target = r
-        self.q_table.loc[s, a] = self.q_table.loc[s, a] + self.lr * (q_target - q_predict)  # update
+        self.q_table.loc[s, a] = self.q_table.loc[s, a] + self.lr * (q_target - q_predict) 
 
 
     def check_state_exist(self, state):
